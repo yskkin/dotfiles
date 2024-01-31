cb1ed6646fb108076b34f59a94228a6b0d1b12be
Yoshiyuki.kinjo <yskkin@gmail.com>
2024-01-31 19:56:30 +0900

    add casks

--- home/Brewfile.v
+++ home/Brewfile.v
@@ -12,6 +12,8 @@ brew "postgresql@11", restart_service: true
 brew "auth0/auth0-cli/auth0"
 brew "azure/functions/azure-functions-core-tools@4"
 
+cask "around"
 cask "mkvtoolnix"
 cask "ngrok"
 cask "vlc"
+cask "zoom"
